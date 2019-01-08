b90953d80784b4e060bc3ed8392e08d65ecc14c8
Mathilde Badoual <mathilde.badoual@gmail.com>
2019-01-08 11:40:36 -0700

    change setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Solver for large scale nonlinear mixed integer problems.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mathildebadoual/Hopfield-NLMIP",
+    url="https://github.com/mathildebadoual/hmip",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
