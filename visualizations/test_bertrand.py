edf9c71644679a8ce04056e06557fa53b14f7459
Mathilde Badoual <mathilde.badoual@gmail.com>
2019-02-04 11:00:09 -0800

    add plots utils

---
+++ visualizations/test_bertrand.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import hmip.hopfield as hop
+from visualizations.utils_visuals import plot_evolution_objective_function_2d
+
+
+H = np.array([[2, 0.1], [0, 1]])
+q = np.array([-2.7, -1.8])
+k_max = 100
+binary_indicator = np.array([0, 1])
+ub = np.array([1, 1])
+lb = np.array([0, 0])
+
+x, x_h, f_val_hist, step_size = hop.hopfield(H, q, lb, ub, binary_indicator, k_max=k_max)
+
+plot_evolution_objective_function_2d(H, q, x, 'plots/objective_function_2d.png')
---
+++ visualizations/utils_visuals.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from hmip.hopfield import objective_function
+
+
+def plot_evolution_objective_function_2d(H, q, x, figure_path):
+    """
+    Plot the evolution of the objective function for 2d variables during the gradient descent
+    :param H: (np.array) size (n, n) from the problem formulation - quadratic parameter
+    :param q: (np.array) size (n,) from the problem formulation - linear parameter
+    :param x: (np.array) size (n, k_max) solution of the problem at each step of the gradient descent
+    :param figure_path: (string) path where to solve the function
+    :return: plot a function and save it in the path
+    """
+    x_1 = np.linspace(0, 1, num=500).reshape((1, -1))
+    x_2 = np.linspace(0, 1, num=500).reshape((1, -1))
+    x_meshgrid_1, x_meshgrid_2 = np.meshgrid(x_1, x_2)
+    x_tot = np.concatenate((x_1, x_2), axis=0)
+    objective = objective_function(x_tot, H, q)
+
+    plt.figure(figsize=(7, 5))
+    plt.contourf(x_meshgrid_1, x_meshgrid_2, objective, 50, cmap='plasma')
+    plt.plot(x[0, :], x[1, :], 'black')
+    plt.colorbar()
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.savefig(figure_path)
+    plt.show()
